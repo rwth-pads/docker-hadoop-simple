92cb0a2131ba5b490801ab59cedbe692e1e182fa
Leah Tacke genannt Unterberg <leah.tgu@pads.rwth-aachen.de>
2024-01-09 21:52:18 +0100

    added commands.txt with info for testing

--- mounted-data/test.sh
+++ mounted-data/test.sh
@@ -1,8 +1,6 @@
-hadoop fs -mkdir /data
-hadoop fs -put ./funny-story.txt /data/funny-story.txt
 hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.10.1.jar \
--files word_mapper.py,word_reducer.py \
--mapper "python word_mapper.py" \
--reducer "python word_reducer.py" \
--input /data/funny-story.txt \
--output /data/counts
+  -files word_mapper.py,word_reducer.py \
+  -mapper "python3 word_mapper.py" \
+  -reducer "python3 word_reducer.py" \
+  -input /data/funny-story.txt \
+  -output /data/counts

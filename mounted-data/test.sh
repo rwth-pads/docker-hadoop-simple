41e166febaeea686a184a65a3cfeb7de24b51047
Leah Tacke genannt Unterberg <leah.tgu@pads.rwth-aachen.de>
2024-01-10 09:26:54 +0100

    line endings; just remove them

--- mounted-data/test.sh
+++ mounted-data/test.sh
@@ -1,6 +1,1 @@
-hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.10.1.jar \
--files word_mapper.py,word_reducer.py \
--mapper "python3 word_mapper.py" \
--reducer "python3 word_reducer.py" \
--input /data/funny-story.txt \
--output /data/counts
+hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.10.1.jar -files word_mapper.py,word_reducer.py -mapper "python3 word_mapper.py" -reducer "python3 word_reducer.py" -input /data/funny-story.txt -output /data/counts
